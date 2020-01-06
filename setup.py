40a72b908dbe7d85fee290d20791d460d9aab25c
Jon Michaelchuck <jmichaelchuck@tableau.com>
2020-01-06 11:45:16 -0800

    Enable pypi publish (#16)

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 import os
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="altimeter",
     version=os.environ.get("PKG_VERSION", "0.0.1dev0+local"),
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
+    author="Tableau",
+    description="Graph AWS resources in Neptune",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tableau/altimeter",
     python_requires=">=3.7,<4",
     install_requires=[
         "aws-requests-auth==0.4.2",
@@ -14,10 +22,17 @@ setup(
         "boto3>=1.9.130",
     ],
     scripts=[
+        "bin/account_scan.py",
+        "bin/aws2json.py",
+        "bin/graphpruner.py",
         "bin/json2rdf.py",
         "bin/rdf2n.py",
-        "bin/aws2json.py",
-        "bin/account_scan.py",
+        "bin/runquery.py",
         "bin/scan_resource.py",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
