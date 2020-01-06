9c4746e49e0bfd79f29fa5ed2b347595e81b31d6
Jon Parten <jparten@tableau.com>
2020-01-06 14:46:44 -0800

    Adding account password policy resource

---
+++ altimeter/aws/resource/iam/account_password_policy.py
@@ -0,0 +1,51 @@
+"""Resource for Account Password Policy"""
+from typing import Type
+
+from botocore.client import BaseClient
+
+from altimeter.aws.resource.resource_spec import ListFromAWSResult
+from altimeter.aws.resource.iam import IAMResourceSpec
+from altimeter.core.graph.field.scalar_field import ScalarField
+from altimeter.core.graph.schema import Schema
+
+
+class AccountPasswordPolicyResourceSpec(IAMResourceSpec):
+    """Resource for Account Password Policy"""
+
+    DEFAULT_PASSWORD_POLICY_NAME = "default"
+
+    type_name = "account-password-policy"
+    schema = Schema(
+        ScalarField("MinimumPasswordLength"),
+        ScalarField("RequireSymbols"),
+        ScalarField("RequireNumbers"),
+        ScalarField("RequireUppercaseCharacters"),
+        ScalarField("RequireLowercaseCharacters"),
+        ScalarField("AllowUsersToChangePassword"),
+        ScalarField("ExpirePasswords"),
+        ScalarField("MaxPasswordAge"),
+        ScalarField("PasswordReusePrevention"),
+        ScalarField("HardExpiry"),
+    )
+
+    @classmethod
+    def list_from_aws(
+            cls: Type["AccountPasswordPolicyResourceSpec"], client: BaseClient, account_id: str, region: str
+    ) -> ListFromAWSResult:
+        """Return a dict of dicts of the format:
+
+            {'account_password_policy_1_arn': {account_password_policy_1_dict}}
+
+        Where the dicts represent results from get_account_password_policy."""
+        password_policies = {}
+        try:
+            resp = client.get_account_password_policy()
+        except client.exceptions.NoSuchEntityException:
+            resp = {}  # Indicates no policy is set for the account
+
+        policy = resp.get("PasswordPolicy", {})
+
+        if policy:
+            resource_arn = cls.generate_arn(account_id, region, cls.DEFAULT_PASSWORD_POLICY_NAME)
+            password_policies[resource_arn] = policy
+        return ListFromAWSResult(resources=password_policies)
---
+++ tests/unit/altimeter/aws/resource/iam/test_account_password_policy.py
@@ -0,0 +1,42 @@
+import unittest
+
+from altimeter.core.resource.resource import Resource
+from altimeter.aws.resource.iam.account_password_policy import AccountPasswordPolicyResourceSpec
+
+
+class TestAccountPasswordPolicyResourceSpec(unittest.TestCase):
+    def test_schema_parse(self):
+        resource_arn = "arn:aws:iam:us-west-2:111122223333:account-password-policy/default"
+        aws_resource_dict = {
+            'MinimumPasswordLength': 12, 'RequireSymbols': True, 'RequireNumbers': True,
+            'RequireUppercaseCharacters': True, 'RequireLowercaseCharacters': True, 'AllowUsersToChangePassword': True,
+            'ExpirePasswords': True, 'MaxPasswordAge': 90, 'PasswordReusePrevention': 5, 'HardExpiry': True
+        }
+
+        links = AccountPasswordPolicyResourceSpec.schema.parse(
+            data=aws_resource_dict, context={"account_id": "111122223333", "region": "us-west-2"}
+        )
+        resource = Resource(
+            resource_id=resource_arn, type_name=AccountPasswordPolicyResourceSpec.type_name, links=links
+        )
+        alti_resource_dict = resource.to_dict()
+
+        expected_alti_resource_dict = {'type': 'account-password-policy',
+                                       'links': [{'pred': 'minimum_password_length', 'obj': 12, 'type': 'simple'},
+                                                 {'pred': 'require_symbols', 'obj': True, 'type': 'simple'},
+                                                 {'pred': 'require_numbers', 'obj': True, 'type': 'simple'}, {
+                                                     'pred': 'require_uppercase_characters', 'obj': True,
+                                                     'type': 'simple'
+                                                 }, {
+                                                     'pred': 'require_lowercase_characters', 'obj': True,
+                                                     'type': 'simple'
+                                                 }, {
+                                                     'pred': 'allow_users_to_change_password', 'obj': True,
+                                                     'type': 'simple'
+                                                 }, {'pred': 'expire_passwords', 'obj': True, 'type': 'simple'},
+                                                 {'pred': 'max_password_age', 'obj': 90, 'type': 'simple'},
+                                                 {'pred': 'password_reuse_prevention', 'obj': 5, 'type': 'simple'},
+                                                 {'pred': 'hard_expiry', 'obj': True, 'type': 'simple'}]
+        }
+
+        self.assertDictEqual(alti_resource_dict, expected_alti_resource_dict)
