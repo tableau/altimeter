7d6a0e908042d2b655ea6a7ee5ec5c8e27242059
Jon Michaelchuck <jmichaelchuck@tableau.com>
2020-04-10 10:17:15 -0700

    handle support

--- altimeter/aws/scan/account_scanner.py
+++ altimeter/aws/scan/account_scanner.py
@@ -126,9 +126,15 @@ class AccountScanner:
                         for resource_spec_class in self.resource_spec_classes:
                             client_name = resource_spec_class.get_client_name()
                             if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
-                                regions_services_resource_spec_classes[random_scan_region][
-                                    client_name
-                                ].append(resource_spec_class)
+                                if resource_spec_class.region_whitelist:
+                                    account_resource_scan_region = resource_spec_class.region_whitelist[
+                                        0
+                                    ]
+                                else:
+                                    account_resource_scan_region = random_scan_region
+                                regions_services_resource_spec_classes[
+                                    account_resource_scan_region
+                                ][client_name].append(resource_spec_class)
                             elif resource_spec_class.scan_granularity == ScanGranularity.REGION:
                                 if resource_spec_class.region_whitelist:
                                     resource_scan_regions = tuple(
