f83856d71ea636f83ba0db159f960be47d6b741b
Jon Michaelchuck <jmichaelchuck@tableau.com>
2020-08-19 09:28:23 -0700

    ci

--- ci/test.sh
+++ ci/test.sh
@@ -19,7 +19,7 @@ export DB_HOST=127.0.0.1
 export DB_NAME=qj_test
 psql -c "create database $DB_NAME;" -U $DB_USER
 export SQLALCHEMY_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}"
-alembic -c /etc/altimeter/qj/alembic/alembic.ini upgrade head
+alembic -c services/qj/alembic/alembic.ini upgrade head
 
 echo "Running tests in $tests_dir against $src_dir and any doctests in $src_dir"
 pytest --ignore=altimeter/qj/alembic/env.py --cov="$src_dir" --cov-report=term-missing --cov-fail-under=${COVERAGE_MIN_PERCENTAGE} --cov-branch --doctest-modules "$src_dir" "$tests_dir"
