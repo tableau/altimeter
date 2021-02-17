60206812fe7e6715dd196f217fa21f2d24da51d9
Jon Michaelchuck <jmichaelchuck@tableau.com>
2021-02-17 11:22:14 -0800

    fix guardduty race condition

--- altimeter/aws/resource/guardduty/detector.py
+++ altimeter/aws/resource/guardduty/detector.py
@@ -2,6 +2,7 @@
 from typing import Any, Dict, List, Type
 
 from botocore.client import BaseClient
+from botocore.exceptions import ClientError
 
 from altimeter.aws.resource.guardduty import GuardDutyResourceSpec
 from altimeter.aws.resource.resource_spec import ListFromAWSResult
@@ -55,57 +56,72 @@ class DetectorResourceSpec(GuardDutyResourceSpec):
         Where the dicts represent results from list_detectors and list_members, get_detector for
         each listed detector."""
         list_detectors_paginator = client.get_paginator("list_detectors")
-        detectors = {}
+        detectors: Dict[str, Dict[str, Any]] = {}
         for list_detectors_resp in list_detectors_paginator.paginate():
             detector_ids = list_detectors_resp["DetectorIds"]
             for detector_id in detector_ids:
-                detector_resp = client.get_detector(DetectorId=detector_id)
-                detector = {
-                    key: detector_resp[key]
-                    for key in (
-                        "CreatedAt",
-                        "FindingPublishingFrequency",
-                        "ServiceRole",
-                        "Status",
-                        "UpdatedAt",
-                    )
-                }
-                list_members_paginator = client.get_paginator("list_members")
-                member_resps: List[Dict[str, Any]] = []
-                for list_members_resp in list_members_paginator.paginate(DetectorId=detector_id):
-                    member_resps += list_members_resp.get("Members", [])
-                members = []
-                if member_resps:
-                    for member_resp in member_resps:
-                        member_account_id = member_resp["AccountId"]
-                        member_detector_id = member_resp["DetectorId"]
-                        member_email = member_resp["Email"]
-                        member_relationship_status = member_resp["RelationshipStatus"]
-                        member_invited_at = member_resp["InvitedAt"]
-                        member_updated_at = member_resp["UpdatedAt"]
-                        member_detector_arn = cls.generate_arn(
-                            account_id=member_account_id,
-                            region=region,
-                            resource_id=member_detector_id,
-                        )
-                        member = {
-                            "DetectorArn": member_detector_arn,
-                            "Email": member_email,
-                            "RelationshipStatus": member_relationship_status,
-                            "InvitedAt": member_invited_at,
-                            "UpdatedAt": member_updated_at,
-                        }
-                        members.append(member)
-                detector["Members"] = members
-                master_account_resp = client.get_master_account(DetectorId=detector_id)
-                master_account_dict = master_account_resp.get("Master")
-                if master_account_dict:
-                    detector["Master"] = {
-                        key: master_account_dict[key]
-                        for key in ("AccountId", "RelationshipStatus", "InvitedAt",)
-                    }
                 resource_arn = cls.generate_arn(
                     account_id=account_id, region=region, resource_id=detector_id
                 )
-                detectors[resource_arn] = detector
+                try:
+                    detectors[resource_arn] = cls.get_detector(client, detector_id, region)
+                except ClientError as c_e:
+                    error_code = getattr(c_e, "response", {}).get("Error", {}).get("Code", {})
+                    if error_code != "BadRequestException":
+                        raise c_e
         return ListFromAWSResult(resources=detectors)
+
+    @classmethod
+    def get_detector(
+        cls: Type["DetectorResourceSpec"], client: BaseClient, detector_id: str, region: str
+    ) -> Dict[str, Any]:
+        detector_resp = client.get_detector(DetectorId=detector_id)
+        detector = {
+            key: detector_resp[key]
+            for key in (
+                "CreatedAt",
+                "FindingPublishingFrequency",
+                "ServiceRole",
+                "Status",
+                "UpdatedAt",
+            )
+        }
+        detector["Members"] = cls.get_detector_members(client, detector_id, region)
+        master_account_resp = client.get_master_account(DetectorId=detector_id)
+        master_account_dict = master_account_resp.get("Master")
+        if master_account_dict:
+            detector["Master"] = {
+                key: master_account_dict[key]
+                for key in ("AccountId", "RelationshipStatus", "InvitedAt",)
+            }
+        return detector
+
+    @classmethod
+    def get_detector_members(
+        cls: Type["DetectorResourceSpec"], client: BaseClient, detector_id: str, region: str
+    ) -> List[Dict[str, Any]]:
+        member_resps: List[Dict[str, Any]] = []
+        list_members_paginator = client.get_paginator("list_members")
+        for list_members_resp in list_members_paginator.paginate(DetectorId=detector_id):
+            member_resps += list_members_resp.get("Members", [])
+        members = []
+        if member_resps:
+            for member_resp in member_resps:
+                member_account_id = member_resp["AccountId"]
+                member_detector_id = member_resp["DetectorId"]
+                member_email = member_resp["Email"]
+                member_relationship_status = member_resp["RelationshipStatus"]
+                member_invited_at = member_resp["InvitedAt"]
+                member_updated_at = member_resp["UpdatedAt"]
+                member_detector_arn = cls.generate_arn(
+                    account_id=member_account_id, region=region, resource_id=member_detector_id,
+                )
+                member = {
+                    "DetectorArn": member_detector_arn,
+                    "Email": member_email,
+                    "RelationshipStatus": member_relationship_status,
+                    "InvitedAt": member_invited_at,
+                    "UpdatedAt": member_updated_at,
+                }
+                members.append(member)
+        return members
