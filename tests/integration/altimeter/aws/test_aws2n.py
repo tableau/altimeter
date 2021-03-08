ac3c23a6ad6c3dbd6b0b89b8ba2053a1da70d524
Jon Michaelchuck <jmichaelchuck@tableau.com>
2021-03-08 13:43:51 -0800

    progress

--- tests/integration/altimeter/aws/test_aws2n.py
+++ tests/integration/altimeter/aws/test_aws2n.py
@@ -13,10 +13,16 @@ from altimeter.aws.auth.accessor import Accessor
 from altimeter.aws.auth.cache import AWSCredentialsCache
 from altimeter.aws.aws2n import aws2n
 from altimeter.aws.resource.awslambda.function import LambdaFunctionResourceSpec
+
 # TODO moto
-#from altimeter.aws.resource.dynamodb.dynamodb_table import DynamoDbTableResourceSpec
+# from altimeter.aws.resource.dynamodb.dynamodb_table import DynamoDbTableResourceSpec
+from altimeter.aws.resource.ec2.flow_log import FlowLogResourceSpec
+from altimeter.aws.resource.ec2.instance import EC2InstanceResourceSpec
+from altimeter.aws.resource.ec2.volume import EBSVolumeResourceSpec
+from altimeter.aws.resource.ec2.subnet import SubnetResourceSpec
 from altimeter.aws.resource.ec2.vpc import VPCResourceSpec
 from altimeter.aws.resource.iam.policy import IAMPolicyResourceSpec
+
 # https://github.com/spulec/moto/pull/3750
 # from altimeter.aws.resource.iam.role import IAMRoleResourceSpec
 from altimeter.aws.resource.s3.bucket import S3BucketResourceSpec
@@ -45,19 +51,35 @@ class TestAWS2NSingleAccount(unittest.TestCase):
                 Filters=[{"Name": "opt-in-status", "Values": ["opt-in-not-required", "opted-in"]}]
             )
             enabled_regions = tuple(region["RegionName"] for region in regions_resp["Regions"])
-            delete_default_vpcs(enabled_regions)
+            delete_vpcs(enabled_regions)
             # add a diverse set of resources which are supported by moto
             ## dynamodb
             # TODO moto is not returning TableId in list/describe
-#            dynamodb_table_1_arn = create_dynamodb_table(
-#                name="test_table_1",
-#                attr_name="test_hash_key_attr_1",
-#                attr_type="S",
-#                key_type="HASH",
-#                region_name=region_name,
-#            )
+            #            dynamodb_table_1_arn = create_dynamodb_table(
+            #                name="test_table_1",
+            #                attr_name="test_hash_key_attr_1",
+            #                attr_type="S",
+            #                key_type="HASH",
+            #                region_name=region_name,
+            #            )
+            ## s3
+            bucket_1_arn = create_bucket(name="test_bucket")
             ## ec2
-            # LEFT OFF HERE - start with create vpc, lots of other things are going to require it.
+            vpc_1_id = create_vpc(cidr_block="10.0.0.0/16", region_name=region_name)
+            subnet_1_id = create_subnet(
+                cidr_block="10.0.0.0/24", vpc_id=vpc_1_id, region_name=region_name
+            )
+            flow_log_1_id = create_flow_log(
+                vpc_id=vpc_1_id, dest_bucket_arn=bucket_1_arn, region_name=region_name
+            )
+            ebs_volume_1_arn = create_volume(
+                size=100, az=f"{region_name}a", region_name=region_name
+            )
+            ami_id = get_ami_id(region_name=region_name)
+            instance_1_arn = create_instance(
+                ami_id=ami_id, subnet_id=subnet_1_id, region_name=region_name
+            )
+            # TODO LEFT OFF HERE
             ## iam
             policy_1_arn = create_iam_policy(
                 name="test_policy_1",
@@ -86,9 +108,6 @@ class TestAWS2NSingleAccount(unittest.TestCase):
                 publish=False,
                 region_name=region_name,
             )
-            ## s3
-            s3_client = boto3.client("s3")
-            s3_client.create_bucket(Bucket="test_bucket")
             test_scan_id = "test_scan_id"
             aws_config = AWSConfig(
                 artifact_path=temp_dir,
@@ -117,11 +136,15 @@ class TestAWS2NSingleAccount(unittest.TestCase):
                 write_master_json=True,
             )
             resource_spec_classes = (
-                #DynamoDbTableResourceSpec, TODO moto
+                # DynamoDbTableResourceSpec, TODO moto
+                EBSVolumeResourceSpec,
+                EC2InstanceResourceSpec,
+                FlowLogResourceSpec,
                 IAMPolicyResourceSpec,
-                #IAMRoleResourceSpec, https://github.com/spulec/moto/pull/3750
+                # IAMRoleResourceSpec, https://github.com/spulec/moto/pull/3750
                 LambdaFunctionResourceSpec,
                 S3BucketResourceSpec,
+                SubnetResourceSpec,
                 VPCResourceSpec,
             )
             muxer = LocalAWSScanMuxer(
@@ -143,58 +166,101 @@ class TestAWS2NSingleAccount(unittest.TestCase):
                     print("*" * 80)
                 raise NotImplementedError
 
+
 # helpers
 
-def delete_default_vpcs(enabled_regions: Iterable[str]) -> None:
-    # remove default vpc
+
+def delete_vpcs(enabled_regions: Iterable[str]) -> None:
     for enabled_region_name in enabled_regions:
         regional_ec2_client = boto3.client("ec2", region_name=enabled_region_name)
-        vpcs_resp = regional_ec2_client.describe_vpcs(
-            Filters=[{"Name": "isDefault", "Values": ["true", ]}, ]
-        )
+        vpcs_resp = regional_ec2_client.describe_vpcs()
         vpcs = vpcs_resp.get("Vpcs", [])
-        if vpcs:
-            if len(vpcs) > 1:
-                raise Exception(f"BUG: more than one vpc: {vpcs}")
-            vpc = vpcs[0]
+        for vpc in vpcs:
             vpc_id = vpc["VpcId"]
-            regional_ec2_client.delete_vpc(VpcId=vpc_id)
+            print("**", vpc_id, regional_ec2_client.delete_vpc(VpcId=vpc_id))
+
 
 ## resource builders
 
 ## dynamodb
 
-def create_dynamodb_table(name: str, attr_name: str, attr_type: str, key_type: str, region_name: str) -> str:
+
+def create_dynamodb_table(
+    name: str, attr_name: str, attr_type: str, key_type: str, region_name: str
+) -> str:
     client = boto3.client("dynamodb", region_name=region_name)
     resp = client.create_table(
         TableName=name,
-        AttributeDefinitions=[
-            {
-                'AttributeName': attr_name,
-                'AttributeType': attr_type,
-            },
-        ],
-        KeySchema=[
-            {
-                'AttributeName': attr_name,
-                'KeyType': key_type,
-            },
-        ],
+        AttributeDefinitions=[{"AttributeName": attr_name, "AttributeType": attr_type,},],
+        KeySchema=[{"AttributeName": attr_name, "KeyType": key_type,},],
     )
     return resp["TableDescription"]["TableName"]
 
+
+## ec2
+
+
+def create_instance(ami_id: str, subnet_id: str, region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.run_instances(ImageId=ami_id, SubnetId=subnet_id, MinCount=1, MaxCount=1)
+    return resp["Instances"][0]["InstanceId"]
+
+
+def get_ami_id(region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.describe_images()
+    return resp["Images"][0]["ImageId"]
+
+
+def create_subnet(cidr_block: str, vpc_id: str, region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.create_subnet(VpcId=vpc_id, CidrBlock=cidr_block,)
+    return resp["Subnet"]["SubnetId"]
+
+
+def create_volume(size: int, az: str, region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.create_volume(Size=size, AvailabilityZone=az,)
+    volume_id = resp["VolumeId"]
+    sts_client = boto3.client("sts")
+    account_id = sts_client.get_caller_identity()["Account"]
+    return f"arn:aws:ec2:{region_name}:{account_id}:volume/{volume_id}"
+
+
+def create_vpc(cidr_block: str, region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.create_vpc(CidrBlock=cidr_block)
+    return resp["Vpc"]["VpcId"]
+
+
+def create_flow_log(vpc_id: str, dest_bucket_arn: str, region_name: str) -> str:
+    client = boto3.client("ec2", region_name=region_name)
+    resp = client.create_flow_logs(
+        ResourceIds=[vpc_id],
+        ResourceType="VPC",
+        TrafficType="ALL",
+        LogDestinationType="s3",
+        LogDestination=dest_bucket_arn,
+        MaxAggregationInterval=600,
+    )
+    flow_log_ids = resp["FlowLogIds"]
+    assert len(flow_log_ids) == 1
+    return flow_log_ids[0]
+
+
 ## iam
+
 
 def create_iam_policy(name: str, policy_doc: Dict[str, Any]) -> str:
     client = boto3.client("iam")
-    resp = client.create_policy(
-        PolicyName=name, PolicyDocument=json.dumps(policy_doc),
-    )
+    resp = client.create_policy(PolicyName=name, PolicyDocument=json.dumps(policy_doc),)
     return resp["Policy"]["Arn"]
 
+
 # https://github.com/spulec/moto/pull/3750
-def create_iam_role(name: str, assume_role_policy_doc: Dict[str, Any],
-                    description: str, max_session_duration: int) -> str:
+def create_iam_role(
+    name: str, assume_role_policy_doc: Dict[str, Any], description: str, max_session_duration: int
+) -> str:
     client = boto3.client("iam")
     resp = client.create_role(
         RoleName=name,
@@ -204,7 +270,9 @@ def create_iam_role(name: str, assume_role_policy_doc: Dict[str, Any],
     )
     return resp["Role"]["Arn"]
 
+
 ## lambda
+
 
 def create_lambda_function(
     name: str,
@@ -230,7 +298,7 @@ def lambda_handler(event, context):
     zip_file.close()
     zip_output.seek(0)
     zip_content = zip_output.read()
-    client  = boto3.client("lambda", region_name=region_name)
+    client = boto3.client("lambda", region_name=region_name)
     resp = client.create_function(
         FunctionName=name,
         Runtime=runtime,
@@ -243,3 +311,10 @@ def lambda_handler(event, context):
         Publish=publish,
     )
     return resp["FunctionArn"]
+
+
+## s3
+def create_bucket(name: str) -> str:
+    client = boto3.client("s3")
+    resp = client.create_bucket(Bucket=name)
+    return f"arn:aws:s3:::{name}"
