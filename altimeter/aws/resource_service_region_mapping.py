3e08d8cdceb6ad23d0b0e2f5f310adffdbabc9c1
Jon Michaelchuck <jmichaelchuck@tableau.com>
2021-03-25 13:57:02 -0700

    tests

--- altimeter/aws/resource_service_region_mapping.py
+++ altimeter/aws/resource_service_region_mapping.py
@@ -1,22 +1,16 @@
 """Discover service/region availability"""
 from collections import defaultdict
-import math
 import random
-from typing import Any, DefaultDict, Dict, List, Mapping, Tuple, Type
+from typing import DefaultDict, Dict, Mapping, Tuple, Type
 from types import MappingProxyType
 
 import boto3
-from pydantic import BaseModel, Field
-import requests
+from pydantic import BaseModel
 
 from altimeter.aws.scan.settings import ALL_RESOURCE_SPEC_CLASSES
 from altimeter.aws.resource.resource_spec import AWSResourceSpec, ScanGranularity
 from altimeter.core.log import Logger
 from altimeter.aws.log_events import AWSLogEvents
-
-
-class UnsupportedServiceRegionMappingVersion(Exception):
-    """Indicates a service region mapping json artifact is using an unsupported version"""
 
 
 class NoRegionsFoundForResource(Exception):
@@ -26,7 +20,6 @@ class NoRegionsFoundForResource(Exception):
 class AWSResourceRegionMappingRepository(BaseModel):
     """Contains the mappings between AWS resources and regions"""
 
-    aws_service_resource_region_mappings: Mapping[str, Mapping[str, Tuple[str, ...]]]
     boto_service_resource_region_mappings: Mapping[str, Mapping[str, Tuple[str, ...]]]
 
     def get_regions(
@@ -37,9 +30,8 @@ class AWSResourceRegionMappingRepository(BaseModel):
             logger.info(event=AWSLogEvents.GetServiceResourceRegionMappingStart)
             service = resource_spec_class.service_name
             resource = resource_spec_class.type_name
-            prefiltered_regions = self.aws_service_resource_region_mappings.get(service, {}).get(
-                resource,
-                self.boto_service_resource_region_mappings.get(service, {}).get(resource, ()),
+            prefiltered_regions = self.boto_service_resource_region_mappings.get(service, {}).get(
+                resource, ()
             )
             if region_whitelist:
                 regions = tuple(
@@ -56,7 +48,6 @@ class AWSResourceRegionMappingRepository(BaseModel):
 
 
 def build_aws_resource_region_mapping_repo(
-    services_regions_json_url: str,
     global_region_whitelist: Tuple[str, ...],
     preferred_account_scan_regions: Tuple[str, ...],
     resource_spec_classes: Tuple[Type[AWSResourceSpec], ...] = ALL_RESOURCE_SPEC_CLASSES,
@@ -64,7 +55,6 @@ def build_aws_resource_region_mapping_repo(
     """Build mappings representing the region availability of AWS Resources.
 
     Args:
-        services_regions_json_url: url of the AWS advertised service/region mapping
         global_region_whitelist: if populated this is used as a region whitelist
         preferred_account_scan_regions: regions which should be used for Account granularity resources
         resource_spec_classes: AWSResourceSpec classes to include in the mappings
@@ -113,7 +103,8 @@ def build_aws_resource_region_mapping_repo(
                     for region in candidate_regions
                     if region in preferred_account_scan_regions
                 )
-                candidate_regions = (random.choice(candidate_regions),)
+                if candidate_regions:
+                    candidate_regions = (random.choice(candidate_regions),)
         raw_boto_service_resource_region_mappings[service_name][resource_name] = candidate_regions
     less_mutable_boto_service_resource_region_mappings: Dict[
         str, Mapping[str, Tuple[str, ...]]
@@ -126,96 +117,9 @@ def build_aws_resource_region_mapping_repo(
         less_mutable_boto_service_resource_region_mappings
     )
 
-    aws_service_resource_region_mappings: Mapping[
-        str, Mapping[str, Tuple[str, ...]]
-    ] = MappingProxyType({})
-
     return AWSResourceRegionMappingRepository(
         boto_service_resource_region_mappings=boto_service_resource_region_mappings,
-        aws_service_resource_region_mappings=aws_service_resource_region_mappings,
     )
-
-
-#
-#        raw_aws_service_region_mappings: Dict[str, List[str]] = {}
-#
-#        logger = Logger()
-#        with logger.bind(services_regions_json_url=services_regions_json_url):
-#            logger.info(event=LogEvent.ServiceRegionDiscoveryStart)
-#            # first try discovering from the advertised regions/services json mapping
-#            try:
-#                raw_aws_service_region_mappings = get_aws_service_region_mapping(
-#                    services=services, services_regions_json_url=services_regions_json_url,
-#                )
-#            except Exception as ex:
-#                # then fall back to using boto
-#                logger.warning(event=LogEvent.ServiceRegionDiscoveryError, error=str(ex))
-#            finally:
-#                logger.info(event=LogEvent.ServiceRegionDiscoveryEnd)
-#
-#        # filter using global_region_whitelist
-#        global_region_whitelist_filtered_aws_service_region_mappings = {
-#            service: [region for region in regions if region in global_region_whitelist]
-#            for service, regions in raw_aws_service_region_mappings.items()
-#        }
-#        global_region_whitelist_filtered_boto_service_region_mappings = {
-#            service: [region for region in regions if region in global_region_whitelist]
-#            for service, regions in raw_boto_service_region_mappings.items()
-#        }
-#        # this is all wrong. this stuff should be by resource not service...
-#        # filter Account ScanGranularity resources using preferred_account_scan_regions
-#        for resource_spec_class in resource_spec_classes:
-#            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
-#                service = resource_spec_class.service_name
-#                resource = resource_spec_class.type_name
-#                if service in global_region_whitelist_filtered_aws_service_region_mappings:
-#                    service_regions = global_region_whitelist_filtered_aws_service_region_mappings[
-#                        service
-#                    ]
-#                    candidate_regions = [
-#                        region
-#                        for region in service_regions
-#                        if region in preferred_account_scan_regions
-#                    ]
-#                    if not candidate_regions:
-#                        raise NoRegionsFoundForResource(
-#                            f"No regions found for resource {service}/{resource}"
-#                        )
-#                    random_region = random.choice(candidate_regions)
-#                    global_region_whitelist_filtered_aws_service_region_mappings[service] = (
-#                        random_region,
-#                    )
-#                if service in global_region_whitelist_filtered_boto_service_region_mappings:
-#                    service_regions = global_region_whitelist_filtered_boto_service_region_mappings[
-#                        service
-#                    ]
-#                    candidate_regions = [
-#                        region
-#                        for region in service_regions
-#                        if region in preferred_account_scan_regions
-#                    ]
-#                    if not candidate_regions:
-#                        raise NoRegionsFoundForResource(
-#                            f"No regions found for resource {service}/{resource}"
-#                        )
-#                    random_region = random.choice(candidate_regions)
-#                    global_region_whitelist_filtered_boto_service_region_mappings[service] = (
-#                        random_region,
-#                    )
-#
-#        # make immutable
-#        aws_service_region_mappings = MappingProxyType(
-#            {
-#                service: tuple(regions)
-#                for service, regions in global_region_whitelist_filtered_aws_service_region_mappings.items()
-#            }
-#        )
-#        boto_service_region_mappings = MappingProxyType(
-#            {
-#                service: tuple(regions)
-#                for service, regions in global_region_whitelist_filtered_boto_service_region_mappings.items()
-#            }
-#        )
 
 
 def get_boto_service_region_mapping(services: Tuple[str, ...]) -> Mapping[str, Tuple[str, ...]]:
@@ -227,49 +131,3 @@ def get_boto_service_region_mapping(services: Tuple[str, ...]) -> Mapping[str, T
             session.get_available_regions(service_name=service, allow_non_regional=True)
         )
     return MappingProxyType(service_region_mapping)
-
-
-# def get_aws_service_region_mapping_json(services_regions_json_url: str) -> Dict[str, Any]:
-#    """Fetch and return the AWS advertised service region mapping."""
-#    region_services_resp = requests.get(services_regions_json_url, timeout=30)
-#    region_services_resp.raise_for_status()
-#    return region_services_resp.json()
-#
-#
-# def get_aws_service_region_mapping(
-#    services: Tuple[str, ...], services_regions_json_url: str
-# ) -> Dict[str, List[str, ...]]:
-#    """Return a mapping of service names to supported regions for the given services using the AWS provided service
-#    region mapping json"""
-#
-#    class RawServiceRegionMapping(BaseModel):
-#        class RawServiceRegionMappingMetadata(BaseModel):
-#            version: float = Field(alias="format:version")
-#
-#        class RawService(BaseModel):
-#            class RawServiceAttributes(BaseModel):
-#                region: str = Field(alias="aws:region")
-#
-#            attributes: RawServiceAttributes
-#            id: str
-#
-#        metadata: RawServiceRegionMappingMetadata
-#        services: List[RawService] = Field(alias="prices")
-#
-#    region_services_json = get_aws_service_region_mapping_json(
-#        services_regions_json_url=services_regions_json_url
-#    )
-#    raw_service_region_mapping = RawServiceRegionMapping(**region_services_json)
-#    expected_major_version: int = 1
-#    major_version = math.floor(raw_service_region_mapping.metadata.version)
-#    if major_version != expected_major_version:
-#        raise UnsupportedServiceRegionMappingVersion(
-#            f"Expected metadata -> format:version major_version {expected_major_version}, got {major_version}"
-#        )
-#    service_region_mapping: DefaultDict[str, List[str]] = defaultdict(list)
-#    for service in raw_service_region_mapping.services:
-#        service_name, service_region = service.id.split(":")
-#        if service_name in services:
-#            service_region_mapping[service_name] += (service_region,)
-#    return service_region_mapping
-#
--- tests/unit/altimeter/aws/test_resource_service_region_mapping.py
+++ tests/unit/altimeter/aws/test_resource_service_region_mapping.py
@@ -1,221 +1,88 @@
-import json
 import unittest
-import unittest.mock
 
-from altimeter.aws.resource_service_region_mapping import AWSResourceRegionMappingRepository
+from altimeter.aws.resource.resource_spec import ScanGranularity
+from altimeter.aws.resource_service_region_mapping import (
+    build_aws_resource_region_mapping_repo,
+    NoRegionsFoundForResource,
+)
+from altimeter.aws.scan.settings import ALL_RESOURCE_SPEC_CLASSES
 
 
 class TestAWSResourceRegionMappingRepository(unittest.TestCase):
-    def test_with_valid_json_data(self):
-        pass
+    def test_with_empty_global_region_whitelist(self):
+        mapping_repo = build_aws_resource_region_mapping_repo(
+            global_region_whitelist=(), preferred_account_scan_regions=("us-east-1",),
+        )
+        for resource_spec_class in ALL_RESOURCE_SPEC_CLASSES:
+            scan_regions = mapping_repo.get_regions(
+                resource_spec_class=resource_spec_class, region_whitelist=()
+            )
+            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
+                self.assertEqual(scan_regions, ("us-east-1",))
+            else:
+                self.assertGreaterEqual(len(scan_regions), 1)
 
+    def test_with_populated_global_region_whitelist(self):
+        mapping_repo = build_aws_resource_region_mapping_repo(
+            global_region_whitelist=("us-east-1", "us-east-2", "us-west-1", "us-west-2"),
+            preferred_account_scan_regions=("us-east-1",),
+        )
+        for resource_spec_class in ALL_RESOURCE_SPEC_CLASSES:
+            scan_regions = mapping_repo.get_regions(
+                resource_spec_class=resource_spec_class, region_whitelist=()
+            )
+            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
+                self.assertEqual(scan_regions, ("us-east-1",))
+            else:
+                self.assertGreaterEqual(len(scan_regions), 1)
+                self.assertLessEqual(len(scan_regions), 4)
 
-# class TestGetAWSServiceRegionMapping(unittest.TestCase):
-#    def test_with_valid_data(self):
-#        services = ("ec2", "elb", "lambda")
-#        sample_data_filepath = "tests/data/services_regions/sample.json"
-#        with open(sample_data_filepath, "r") as fp:
-#            region_services_json = json.load(fp)
-#        expected_aws_service_region_mapping = {
-#            "ec2": (
-#                "af-south-1",
-#                "ap-northeast-3",
-#                "ap-south-1",
-#                "eu-north-1",
-#                "eu-west-2",
-#                "sa-east-1",
-#                "us-gov-west-1",
-#                "ap-northeast-2",
-#                "ap-southeast-1",
-#                "cn-north-1",
-#                "eu-south-1",
-#                "us-east-1",
-#                "us-east-2",
-#                "us-gov-east-1",
-#                "us-west-2",
-#                "ca-central-1",
-#                "cn-northwest-1",
-#                "eu-central-1",
-#                "us-west-1",
-#            ),
-#            "elb": (
-#                "ap-northeast-1",
-#                "ap-northeast-3",
-#                "ap-south-1",
-#                "cn-north-1",
-#                "eu-south-1",
-#                "eu-west-2",
-#                "me-south-1",
-#                "us-east-2",
-#                "us-west-2",
-#                "af-south-1",
-#                "ap-northeast-2",
-#                "cn-northwest-1",
-#                "eu-central-1",
-#                "eu-west-1",
-#                "eu-west-3",
-#                "us-east-1",
-#                "us-gov-west-1",
-#                "ap-southeast-1",
-#                "ap-southeast-2",
-#                "ca-central-1",
-#                "us-west-1",
-#            ),
-#            "lambda": (
-#                "ap-east-1",
-#                "ap-northeast-2",
-#                "ap-south-1",
-#                "ap-southeast-1",
-#                "ap-southeast-2",
-#                "me-south-1",
-#                "us-east-1",
-#                "us-east-2",
-#                "us-gov-west-1",
-#                "ap-northeast-1",
-#                "ca-central-1",
-#                "eu-north-1",
-#                "eu-south-1",
-#                "eu-west-1",
-#                "eu-west-3",
-#                "sa-east-1",
-#                "us-west-2",
-#                "af-south-1",
-#                "ap-northeast-3",
-#                "cn-northwest-1",
-#                "eu-central-1",
-#            ),
-#        }
-#        aws_service_region_mapping = get_aws_service_region_mapping(
-#            services=services, region_services_json=region_services_json
-#        )
-#        self.assertDictEqual(
-#            expected_aws_service_region_mapping, dict(aws_service_region_mapping),
-#        )
-#
-#    def test_with_unsupported_version(self):
-#        services = ("ec2", "elb", "lambda")
-#        sample_data_filepath = "tests/data/services_regions/sample_unsupported_version.json"
-#        with open(sample_data_filepath, "r") as fp:
-#            region_services_json = json.load(fp)
-#        with self.assertRaises(UnsupportedServiceRegionMappingVersion):
-#            get_aws_service_region_mapping(
-#                services=services, region_services_json=region_services_json
-#            )
-#
-#
-# class TestGetServiceRegionMapping(unittest.TestCase):
-#    def test_with_valid_json_data(self):
-#        services = ("ec2", "elb", "lambda")
-#        sample_data_filepath = "tests/data/services_regions/sample.json"
-#        with open(sample_data_filepath, "r") as fp:
-#            region_services_json = json.load(fp)
-#        with unittest.mock.patch(
-#            "altimeter.aws.scan.services_regions.get_aws_service_region_mapping_json"
-#        ) as mock_get_aws_service_region_mapping_json:
-#            mock_get_aws_service_region_mapping_json.return_value = region_services_json
-#            service_region_mapping = get_service_region_mapping(
-#                services=services, services_regions_json_url="mocked"
-#            )
-#            expected_service_region_mapping = {
-#                "ec2": (
-#                    "af-south-1",
-#                    "ap-northeast-3",
-#                    "ap-south-1",
-#                    "eu-north-1",
-#                    "eu-west-2",
-#                    "sa-east-1",
-#                    "us-gov-west-1",
-#                    "ap-northeast-2",
-#                    "ap-southeast-1",
-#                    "cn-north-1",
-#                    "eu-south-1",
-#                    "us-east-1",
-#                    "us-east-2",
-#                    "us-gov-east-1",
-#                    "us-west-2",
-#                    "ca-central-1",
-#                    "cn-northwest-1",
-#                    "eu-central-1",
-#                    "us-west-1",
-#                ),
-#                "elb": (
-#                    "ap-northeast-1",
-#                    "ap-northeast-3",
-#                    "ap-south-1",
-#                    "cn-north-1",
-#                    "eu-south-1",
-#                    "eu-west-2",
-#                    "me-south-1",
-#                    "us-east-2",
-#                    "us-west-2",
-#                    "af-south-1",
-#                    "ap-northeast-2",
-#                    "cn-northwest-1",
-#                    "eu-central-1",
-#                    "eu-west-1",
-#                    "eu-west-3",
-#                    "us-east-1",
-#                    "us-gov-west-1",
-#                    "ap-southeast-1",
-#                    "ap-southeast-2",
-#                    "ca-central-1",
-#                    "us-west-1",
-#                ),
-#                "lambda": (
-#                    "ap-east-1",
-#                    "ap-northeast-2",
-#                    "ap-south-1",
-#                    "ap-southeast-1",
-#                    "ap-southeast-2",
-#                    "me-south-1",
-#                    "us-east-1",
-#                    "us-east-2",
-#                    "us-gov-west-1",
-#                    "ap-northeast-1",
-#                    "ca-central-1",
-#                    "eu-north-1",
-#                    "eu-south-1",
-#                    "eu-west-1",
-#                    "eu-west-3",
-#                    "sa-east-1",
-#                    "us-west-2",
-#                    "af-south-1",
-#                    "ap-northeast-3",
-#                    "cn-northwest-1",
-#                    "eu-central-1",
-#                ),
-#            }
-#            self.assertDictEqual(
-#                expected_service_region_mapping, dict(service_region_mapping),
-#            )
-#
-#    def test_with_unsupported_version_json_data(self):
-#        services = ("ec2", "elb", "lambda")
-#        sample_data_filepath = "tests/data/services_regions/sample_unsupported_version.json"
-#        with open(sample_data_filepath, "r") as fp:
-#            region_services_json = json.load(fp)
-#        with unittest.mock.patch(
-#            "altimeter.aws.scan.services_regions.get_aws_service_region_mapping_json"
-#        ) as mock_get_aws_service_region_mapping_json:
-#            mock_get_aws_service_region_mapping_json.return_value = region_services_json
-#            with unittest.mock.patch(
-#                "boto3.Session.get_available_regions"
-#            ) as mock_boto_session_get_available_regions:
-#                mock_boto_session_get_available_regions.return_value = [
-#                    "us-east-1",
-#                    "us-east-2",
-#                    "us-west-1",
-#                    "us-west-2",
-#                ]
-#                service_region_mapping = get_service_region_mapping(
-#                    services=services, services_regions_json_url="mocked"
-#                )
-#                expected_service_region_mapping = {
-#                    "ec2": ("us-east-1", "us-east-2", "us-west-1", "us-west-2",),
-#                    "elb": ("us-east-1", "us-east-2", "us-west-1", "us-west-2",),
-#                    "lambda": ("us-east-1", "us-east-2", "us-west-1", "us-west-2",),
-#                }
-#                self.assertDictEqual(
-#                    expected_service_region_mapping, dict(service_region_mapping),
-#                )
-#
+    def test_with_under_populated_global_region_whitelist(self):
+        mapping_repo = build_aws_resource_region_mapping_repo(
+            global_region_whitelist=("us-east-2", "us-west-1", "us-west-2"),
+            preferred_account_scan_regions=("us-east-1",),
+        )
+        for resource_spec_class in ALL_RESOURCE_SPEC_CLASSES:
+            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
+                with self.assertRaises(NoRegionsFoundForResource):
+                    mapping_repo.get_regions(
+                        resource_spec_class=resource_spec_class, region_whitelist=()
+                    )
+            else:
+                scan_regions = mapping_repo.get_regions(
+                    resource_spec_class=resource_spec_class, region_whitelist=()
+                )
+                self.assertGreaterEqual(len(scan_regions), 1)
+                self.assertLessEqual(len(scan_regions), 4)
+
+    def test_with_populated_account_region_whitelist(self):
+        mapping_repo = build_aws_resource_region_mapping_repo(
+            global_region_whitelist=("us-east-1", "us-east-2", "us-west-1", "us-west-2"),
+            preferred_account_scan_regions=("us-east-1",),
+        )
+        for resource_spec_class in ALL_RESOURCE_SPEC_CLASSES:
+            scan_regions = mapping_repo.get_regions(
+                resource_spec_class=resource_spec_class, region_whitelist=("us-east-1", "us-east-2")
+            )
+            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
+                self.assertEqual(scan_regions, ("us-east-1",))
+            else:
+                self.assertGreaterEqual(len(scan_regions), 1)
+                self.assertLessEqual(len(scan_regions), 2)
+
+    def test_with_under_populated_account_region_whitelist(self):
+        mapping_repo = build_aws_resource_region_mapping_repo(
+            global_region_whitelist=("us-east-1", "us-east-2", "us-west-1", "us-west-2"),
+            preferred_account_scan_regions=("us-east-1",),
+        )
+        for resource_spec_class in ALL_RESOURCE_SPEC_CLASSES:
+            if resource_spec_class.scan_granularity == ScanGranularity.ACCOUNT:
+                with self.assertRaises(NoRegionsFoundForResource):
+                    mapping_repo.get_regions(
+                        resource_spec_class=resource_spec_class, region_whitelist=("us-east-2")
+                    )
+            else:
+                scan_regions = mapping_repo.get_regions(
+                    resource_spec_class=resource_spec_class, region_whitelist=("us-east-2")
+                )
+                self.assertEqual(len(scan_regions), 1)
