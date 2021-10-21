6a190ead4f2e18d3c9e5b5a6ec53045f0ef2d0ca
Jon Michaelchuck <jmichaelchuck@tableau.com>
2021-10-21 09:57:25 -0700

    handle remediator lambda

--- bin/queryjob_lambda.py
+++ bin/queryjob_lambda.py
@@ -1,11 +1,14 @@
-"""Execute all known QJs, run the query portion of a QJ, and prune results according to Job config settings"""
+"""Execute all known QJs, run the query portion of a QJ, remediate a QJ and prune results according to Job config
+settings"""
 import os.path
+
 from typing import Any, Dict
 
 from altimeter.qj.config import QJHandlerConfig
 from altimeter.qj.lambdas.executor import executor
-from altimeter.qj.lambdas.query import query
 from altimeter.qj.lambdas.pruner import pruner
+from altimeter.qj.lambdas.query import query
+from altimeter.qj.lambdas.remediator import remediator
 
 
 class InvalidLambdaModeException(Exception):
@@ -21,6 +24,8 @@ def lambda_handler(event: Dict[str, Any], _: Any) -> None:
         query(event)
     elif handler.mode == "pruner":
         pruner()
+    elif handler.mode == "remediator":
+        remediator(event)
     else:
         raise InvalidLambdaModeException(
             f"Invalid lambda MODE value.\nENV: {os.environ}\nEvent: {event}"
